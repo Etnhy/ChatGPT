ec74430689991e1874b53c456873f50dd18d873b
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-03-25 12:32:45 +0200

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Евгений Маглена on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
