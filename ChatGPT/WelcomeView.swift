45d4379e449715746eba36200723e7e6428e110c
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-03-28 01:14:22 +0300

    welcome view ui refactor

--- ChatGPT/WelcomeView.swift
+++ ChatGPT/WelcomeView.swift
@@ -11,31 +11,45 @@ struct WelcomeView: View {
     @EnvironmentObject var vm: ChatGPTViewModel
     
     var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
             VStack {
-                Image("openai2")
-                    .resizable()
-                    .frame(width: 300, height: 80)
-                    .foregroundColor(.black)
-                    .padding()
-
-                HStack {
-                    TextField("Enter OpenAI key", text: $vm.apiKey)
-                        .padding()
-                        .background(.gray.opacity(0.3))
-                        .cornerRadius(12)
-                    Button {
-                        vm.setApiKey()
-                    } label: {
-                        Text("Set")
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(.black)
-                            .cornerRadius(12)
+                HStack(alignment: .center) {
+                    Image("openai")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                    VStack(alignment: .leading) {
+                        Text("Open AI")
+                            .font(.system(size: 60))
+                            .bold()
+                        Text("Chat GPT")
+                            .font(.system(size: 30))
+                            .bold()
                     }
-                }                    
+                    .foregroundColor(.white)
+                }
+                    HStack {
+                        TextField("Enter OpenAI key", text: $vm.apiKey)
+                            .padding()
+                            .background(.gray.opacity(0.3))
+                            .cornerRadius(12)
+                        Button {
+                            vm.setApiKey()
+                        } label: {
+                            Text("Set")
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(.green.opacity(0.7))
+                                .cornerRadius(12)
+                        }
+                    }
             }
-            .padding()
+                .padding()
+        }
+//        .background(Color.black, ignoresSafeAreaEdges: .all)
     }
+    
 }
 
 struct WelcomeView_Previews: PreviewProvider {
